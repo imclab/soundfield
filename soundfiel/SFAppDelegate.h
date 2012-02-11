14489678c544a9021da46d843adaead902eb85fe
Tom MacWright <tom@macwright.org>
2012-02-10 21:56:43 -0500

    Initial Commit

---
+++ soundfiel/SFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SFAppDelegate.h
+//  soundfiel
+//
+//  Created by Tom MacWright on 2/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ soundfiel/SFViewController.h
@@ -0,0 +1,13 @@
+//
+//  SFViewController.h
+//  soundfiel
+//
+//  Created by Tom MacWright on 2/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SFViewController : UIViewController
+
+@end
